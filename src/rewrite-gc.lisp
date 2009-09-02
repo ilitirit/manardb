39e00682aa017c34956e39b94d4a6c4b8ca66c13
John Fremlin <jf@msi.co.jp>
2009-09-02 19:46:05 +0900

    optionally print progress in gc-rewrite

--- src/rewrite-gc.lisp
+++ src/rewrite-gc.lisp
@@ -1,53 +1,65 @@
 (in-package #:manardb)
 
-(defun rewrite-gc-walk (root-objects-sequence shared-tables new-mtagmaps)
+(defun rewrite-gc-walk (root-objects-sequence shared-tables new-mtagmaps &key progress)
   (declare (optimize speed))
   (macrolet ((vref (mptr)
 	       `(gethash (mptr-index ,mptr) 
 			 (aref (the simple-vector visited) (mptr-tag ,mptr)))))
-    (iter 
-      (for o in-sequence root-objects-sequence)
-      (let ((visited (map 'vector (lambda (x table) (or table (when x (make-hash-table :test 'eql)))) 
-			  new-mtagmaps shared-tables)))
-	(declare (dynamic-extent visited))
-	(labels ((walk-ref (mptr referrer)
-		   (declare (type mptr mptr))
-		   (cond ((zerop mptr) 0)
-			 ((vref mptr))
-			 (t
-			  (let* ((tag (mptr-tag mptr)) 
-				 (mtagmap (aref new-mtagmaps tag))
-				 (len (mtagmap-elem-len mtagmap))
-				 (new-index (mtagmap-alloc mtagmap len))
-				 (new-mptr (make-mptr tag new-index))
-				 (old-mptr mptr))
+    (let* ((print-step
+	    (when progress
+	      (ceiling (length root-objects-sequence) (if (numberp progress) progress 10))))
+	   (print-next print-step)
+	   (start-time (get-internal-real-time)))
 
-			    (osicat-posix:memcpy 
-			     (cffi:inc-pointer (mtagmap-ptr mtagmap)
-					       new-index)
-			     (mptr-pointer mptr)
-			     len)
+      (iter 
+	(for o in-sequence root-objects-sequence)
+	(for count from 0)
+	(when (and print-next (= count print-next))
+	  (format t "~&Added ~D objects; ~$ object/s~%" count
+		  (/ (* count internal-time-units-per-second) (- (get-internal-real-time) start-time)))
+	  (incf print-next print-step))
+      
+	(let ((visited (map 'vector (lambda (x table) (or table (when x (make-hash-table :test 'eql)))) 
+			    new-mtagmaps shared-tables)))
+	  (declare (dynamic-extent visited))
+	  (labels ((walk-ref (mptr referrer)
+		     (declare (type mptr mptr))
+		     (cond ((zerop mptr) 0)
+			   ((vref mptr))
+			   (t
+			    (let* ((tag (mptr-tag mptr)) 
+				   (mtagmap (aref new-mtagmaps tag))
+				   (len (mtagmap-elem-len mtagmap))
+				   (new-index (mtagmap-alloc mtagmap len))
+				   (new-mptr (make-mptr tag new-index))
+				   (old-mptr mptr))
+
+			      (osicat-posix:memcpy 
+			       (cffi:inc-pointer (mtagmap-ptr mtagmap)
+						 new-index)
+			       (mptr-pointer mptr)
+			       len)
 				
-			    (unless (zerop referrer) ;; XXX slight hack to stop the hash-tables growing large by only looking at the first element of an array
-			      (setf (vref old-mptr) new-mptr))
-			    (let ((walker (mtagmap-walker (mtagmap (mptr-tag mptr)))))
-			      (when walker
-				(labels ((reset-ref (mptr referrer)
-					   (cond 
-					     ((zerop referrer) (walk-ref mptr referrer))
-					     (t
-					      (let ((offset (- (mptr-index referrer) (mptr-index old-mptr)))
-						    (new-mptr (walk-ref mptr referrer)))
-						(setf (d (cffi:inc-pointer (mtagmap-ptr mtagmap)
-									   (+ offset new-index)) 0 mptr)
-						      new-mptr))))))
-				  (declare (dynamic-extent #'reset-ref))
-				  (funcall walker mptr #'reset-ref))))
+			      (unless (zerop referrer) ;; XXX slight hack to stop the hash-tables growing large by only looking at the first element of an array
+				(setf (vref old-mptr) new-mptr))
+			      (let ((walker (mtagmap-walker (mtagmap (mptr-tag mptr)))))
+				(when walker
+				  (labels ((reset-ref (mptr referrer)
+					     (cond 
+					       ((zerop referrer) (walk-ref mptr referrer))
+					       (t
+						(let ((offset (- (mptr-index referrer) (mptr-index old-mptr)))
+						      (new-mptr (walk-ref mptr referrer)))
+						  (setf (d (cffi:inc-pointer (mtagmap-ptr mtagmap)
+									     (+ offset new-index)) 0 mptr)
+							new-mptr))))))
+				    (declare (dynamic-extent #'reset-ref))
+				    (funcall walker mptr #'reset-ref))))
 				    
-			    new-mptr)))))
-	  (declare (dynamic-extent #'walk-ref))
-	  (let ((mptr (force-mptr o)))
-	    (walk-ref mptr mptr)))))))
+			      new-mptr)))))
+	    (declare (dynamic-extent #'walk-ref))
+	    (let ((mptr (force-mptr o)))
+	      (walk-ref mptr mptr))))))))
 
 (defun rewrite-gc-cleanup (new-mtagmaps new-files)
   (loop for new across new-mtagmaps
@@ -61,7 +73,7 @@
 	   (osicat-posix:rename new-file old-file)
 	   (mtagmap-open old)))))
 
-(defun rewrite-gc (root-objects-sequence &key verbose shared-classes (base-shared-classes '(mm-symbol mm-string mm-array marray mm-fixed-string)))
+(defun rewrite-gc (root-objects-sequence &key progress verbose shared-classes (base-shared-classes '(mm-symbol mm-string mm-array marray mm-fixed-string)))
   (check-mmap-truncate-okay)
   (let* ((new-mtagmaps
 	 (map '(vector mtagmap) (lambda (m)
@@ -91,7 +103,7 @@
 		 (when m
 		   (ignore-errors (delete-file f))
 		   (mtagmap-open m :file f :finalize nil)))
-	   (rewrite-gc-walk root-objects-sequence shared-tables new-mtagmaps)
+	   (rewrite-gc-walk root-objects-sequence shared-tables new-mtagmaps :progress progress)
 	   (when verbose
 	     (loop for new across new-mtagmaps
 		   for old across *mtagmaps*
