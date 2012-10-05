ea380b6440669619bc0b8ad499102acc1b3f4250
dan lentz <danlentz@gmail.com>
2012-10-05 05:39:08 -0400

    correct iterate clause [hashtable uses :in-hashtable not :in]

--- t/symbol.lisp
+++ t/symbol.lisp
@@ -17,7 +17,8 @@
        (is (= mptr (lisp-object-to-mptr keyword)))))
 
 
-(deftest store-all-symbols-test (&optional (packages (list (find-package :cl) (find-package :mm))))
+(deftest store-all-symbols-test (&optional (packages (list (find-package :cl)
+                                                       (find-package :manardb))))
   (macrolet ((do-all-syms ((var) &body body)
 	       (alexandria:with-gensyms (package)
 		 `(loop for ,package in packages do
@@ -32,11 +33,11 @@
 		   (setf (gethash sym table) mptr))))
           (do-all-syms (sym)
             (add sym (lisp-object-to-mptr sym)))
-          (iter (for (sym mptr) in table)
+          (iter (for (sym mptr) :in-hashtable table)
             (is (= mptr (lisp-object-to-mptr sym))))
           (do-all-syms (sym)
             (is (eq sym (mptr-to-lisp-object (lisp-object-to-mptr sym)))))
-          (iter (for (sym mptr) in table)
+          (iter (for (sym mptr) :in-hashtable table)
             (is (eq sym (mptr-to-lisp-object mptr))))
           (do-all-syms (sym)
             (add sym (lisp-object-to-mptr sym))))))))
